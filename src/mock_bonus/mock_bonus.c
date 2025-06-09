e2f9162859cdadc707b0284986ec049c9e1eedf8
Thomas Anatol da Rocha Woelz <twoelz@gmail.com>
2025-06-09 15:15:57 +0200

    reorg

--- src/mock_bonus/mock_bonus.c
+++ src/mock_bonus/mock_bonus.c
@@ -6,39 +6,13 @@
 /*   By: tda-roch <tda-roch@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 13:24:17 by tda-roch          #+#    #+#             */
-/*   Updated: 2025/06/09 00:37:18 by tda-roch         ###   ########.fr       */
+/*   Updated: 2025/06/09 15:15:13 by tda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 /*empty bonus functions*/
-
-void	so_long_bonus(t_game_data *g)
-{
-	(void)g;
-}
-
-int	init_game_data_bonus(t_game_data *g)
-{
-	(void)g;
-	return (0);
-}
-
-void	exit_game_reached_bonus(t_game_data *g)
-{
-	(void)g;
-}
-
-void	check_chars_bonus(t_game_data *g)
-{
-	(void)g;
-}
-
-void	load_game_images_bonus(t_game_data *g)
-{
-	(void)g;
-}
 
 void	add_game_tile_bonus(t_game_data *g, int x, int y, char c)
 {
@@ -48,14 +22,19 @@ void	add_game_tile_bonus(t_game_data *g, int x, int y, char c)
 	(void)c;
 }
 
-void	z_position_tiles_bonus(t_game_data *g)
+void	bonus_tiles_cleanup(t_game_data *g)
 {
 	(void)g;
 }
 
-void	game_loop_bonus(void *param)
+void	check_chars_bonus(t_game_data *g)
 {
-	(void)param;
+	(void)g;
+}
+
+void	exit_game_reached_bonus(t_game_data *g)
+{
+	(void)g;
 }
 
 void	free_everything_bonus(t_game_data *g)
@@ -63,12 +42,24 @@ void	free_everything_bonus(t_game_data *g)
 	(void)g;
 }
 
-void	bonus_tiles_cleanup(t_game_data *g)
+void	game_key_hook_bonus(t_game_data *g, keys_t key)
 {
 	(void)g;
+	(void)key;
 }
 
-void	process_position_bonus(t_game_data *g)
+void	game_loop_bonus(void *param)
+{
+	(void)param;
+}
+
+int	init_game_data_bonus(t_game_data *g)
+{
+	(void)g;
+	return (0);
+}
+
+void	load_game_images_bonus(t_game_data *g)
 {
 	(void)g;
 }
@@ -79,8 +70,17 @@ void	process_collect_bonus(t_game_data *g, size_t i)
 	(void)i;
 }
 
-void	game_key_hook_bonus(t_game_data *g, keys_t key)
+void	process_position_bonus(t_game_data *g)
 {
 	(void)g;
-	(void)key;
+}
+
+void	so_long_bonus(t_game_data *g)
+{
+	(void)g;
+}
+
+void	z_position_tiles_bonus(t_game_data *g)
+{
+	(void)g;
 }
